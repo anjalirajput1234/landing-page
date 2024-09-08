defc1c70ab01d3f774eb98bf69ddd76011b9a430
anjali rajput1234 <mahendrasinghm380@gmail.com>
2024-09-08 10:38:55 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+console.log("WELCOME DECENTRALIZED CONTENT SHARING PLATFORM")
+var a = document.querySelector("h1")
+
+a.addEventListener("click",function(){
+    a.innerHTML=<div class="sidebar">
+    <a href="#home">Home</a>
+    <a href="#services">Services</a>
+    <a href="#contact">Contact</a></div>
+})
